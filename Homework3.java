b9f21bec5a3f5f2a1abe5e6b7fb4264e68a2de83
Chozza1 <78664784+Chozza1@users.noreply.github.com>
2021-02-14 11:43:19 +0300

    Update Homework3.java

--- Homework3.java
+++ Homework3.java
@@ -1,22 +1,37 @@
 import java.util.Scanner;
 
 public class Homework3 {
-    static Scanner gues = new Scanner(System.in);
     public static void main(String[] args) {
         System.out.println("Угадайте число от 0 до 9");
+        GuesNumber();
+    }
+    static Scanner gues = new Scanner(System.in);
+    public static void GuesNumber() {
         int a = (int) (Math.random() * 10);
         for (int i = 0; i < 3; i++) {
-            System.out.println("Осталось попыток " + (3 - i));
+            System.out.println("Осталось попыток: " + (3 - i));
             int answer = gues.nextInt();
             if (answer == a) {
                 System.out.println("Вы угадали!");
                 break;
             } else if (answer > a) {
                 System.out.println("Загаданое число меньше");
-            } else if (answer < a) {
+            } else {
                 System.out.println("Загаданное число больше");
             }
         }
+        GuesInt();
     }
-}
-//задать выбор после окончания игры 1 -играть еще раз, 2 - выйти ((switch, 2 метода, метка??)
+    static Scanner choise = new Scanner(System.in);
+    public static void GuesInt(){
+        System.out.println("Хотите сыграть еще раз? 1 - да ; 2 - нет");
+            int a = choise.nextInt();
+            if (a == 1) {
+                GuesNumber();
+            }
+            else {
+                System.out.println("Спасибо что играли");
+            }
+        }
+
+    }

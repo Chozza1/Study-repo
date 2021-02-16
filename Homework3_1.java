642ee24f2812a8e3b9e127faccff48f851789e95
Chozza1 <78664784+Chozza1@users.noreply.github.com>
2021-02-16 23:49:43 +0300

    Update Homework3_1.java

--- Homework3_1.java
+++ Homework3_1.java
@@ -1,41 +1,71 @@
+
 import java.util.Scanner;
 
-public class Homework3_1 {
-    static Scanner gues = new Scanner(System.in);
+public class Homework3 {
     public static void main(String[] args) {
-    GuesWord();
+        WordIn();
     }
-    public static void GuesWord(){
-        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
-        String word = words[(int) (Math.random() * words.length)];
-        for (int i = 0; i < Integer.MAX_VALUE; i++){
-            String answer = gues.nextLine();
-            for (int j = 0; j <= answer.length(); j++){
-                String f = "###############";
-                char a = word.charAt(j);
-                char c = answer.charAt(j);
-                char b = f.charAt(j);
-                if (a == c) {
-                    b = c;
-                    char d =f.charAt(j);
-                    d=b;
-                    System.out.println(f);
-                }
-                else {
-                    break;
-                }
-            }
-            if (word.equals(answer));{
-                System.out.println("Вы угадали " + word);
+
+    static Scanner gues = new Scanner(System.in);
+
+    public static void GuesNumber() {
+        System.out.println("Угадайте число от 0 до 9");
+        int a = (int) (Math.random() * 10);
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Осталось попыток: " + (3 - i));
+            int answer = gues.nextInt();
+            if (answer == a) {
+                System.out.println("Вы угадали!");
                 break;
+            } else if (answer > a) {
+                System.out.println("Загаданое число меньше");
+            } else {
+                System.out.println("Загаданное число больше");
             }
-
         }
+        RePlay();
+    }
 
-        //15 char-of gj arr?
-        //for  while
-        // String str = "apple";
-        //char a = str.charAt(0)
+    static Scanner choise = new Scanner(System.in);
+    public static void RePlay() {
+        System.out.println("Хотите сыграть еще раз? 1 - да ; 2 - нет");
+        int a = choise.nextInt();
+        if (a == 1) {
+            GuesNumber();
+        } else {
+            System.out.println("Спасибо что играли");
+        }
+    }
+
+    static Scanner gues1 = new Scanner(System.in);
+    public static void GuesWord(String wordIn) {
+        String[] words = {"apple", "orange", "lemon", "banana", "apricot", "avocado", "broccoli", "carrot", "cherry", "garlic", "grape", "melon", "leak", "kiwi", "mango", "mushroom", "nut", "olive", "pea", "peanut", "pear", "pepper", "pineapple", "pumpkin", "potato"};
+        char[] block = new char[15];
+        for (int n = 0; n < block.length - 1; n++) {
+            String block1 = "#";
+            char b = block1.charAt(0);
+            block[n] = b;
+        }
+        String word = words[(int) (Math.random() * words.length)];
+        System.out.println(block);
+            for (int j = 0; j < block.length-1; j++) {
+               if (word.length() < wordIn.length()){
+                   char a = word.charAt(j);
+                   char c = wordIn.charAt(j);
+                   if (a == c){
+                       block[j] = c;
+                       System.out.println(block);
+                   }
+
+                }
+            }
+        WordIn();
+    }
+
+
+    public static void WordIn() {
+        System.out.println("Угадайте слово");
+        String answer = gues1.nextLine();
+        GuesWord(answer);
     }
 }
-
